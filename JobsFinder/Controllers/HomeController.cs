3978635be4f143ecba71ebd7108b9ae8b949da01
Prem Parmar <premparmar1997@gmail.com>
2023-01-17 22:20:49 +0530

    Test

--- JobsFinder/Controllers/HomeController.cs
+++ JobsFinder/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace JobsFinder.Controllers
     public class HomeController : Controller
     {
         //
-        // GET: /Home/prem
+        // GET: /Home/
 
         public ActionResult Index()
         {
